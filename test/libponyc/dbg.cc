73a9aaa2ece60f352bce3aee65fa93726ac7e465
Wink Saville <wink@saville.com>
2018-02-05 08:44:53 -0800

    Cleanup tests dbg.cc
    Change most of the ASSERT's to EXPECTS's and rename dummy to second
    and reorder some routines.

--- test/libponyc/dbg.cc
+++ test/libponyc/dbg.cc
@@ -15,194 +15,13 @@
  */
 enum {
   DBG_BITS_FIRST(first, 30),
-  DBG_BITS_NEXT(dummy, 3, first),
-  DBG_BITS_NEXT(another, 2, dummy),
+  DBG_BITS_NEXT(second, 3, first),
+  DBG_BITS_NEXT(another, 2, second),
   DBG_BITS_SIZE(bits_size, another)
 };
 
 class DbgTest : public testing::Test
 {};
-
-TEST_F(DbgTest, DbgBnoi)
-{
-  ASSERT_EQ(dbg_bnoi(first,0), 0);
-  ASSERT_EQ(dbg_bnoi(first,1), 1);
-  ASSERT_EQ(dbg_bnoi(dummy,0), 30);
-  ASSERT_EQ(dbg_bnoi(dummy,1), 31);
-  ASSERT_EQ(dbg_bnoi(dummy,2), 32);
-  ASSERT_EQ(dbg_bnoi(another,0), 33);
-  ASSERT_EQ(dbg_bnoi(another,1), 34);
-  ASSERT_EQ(bits_size, 30 + 3 + 2);
-}
-
-TEST_F(DbgTest, DbgBitsArrayIdx)
-{
-  ASSERT_EQ(_DBG_BITS_ARRAY_IDX(0), 0);
-  ASSERT_EQ(_DBG_BITS_ARRAY_IDX(31), 0);
-  ASSERT_EQ(_DBG_BITS_ARRAY_IDX(32), 1);
-  ASSERT_EQ(_DBG_BITS_ARRAY_IDX(63), 1);
-  ASSERT_EQ(_DBG_BITS_ARRAY_IDX(64), 2);
-  ASSERT_EQ(_DBG_BITS_ARRAY_IDX(95), 2);
-}
-
-TEST_F(DbgTest, DbgBitMask)
-{
-  ASSERT_EQ(_DBG_BIT_MASK(0),  0x00000001);
-  ASSERT_EQ(_DBG_BIT_MASK(1),  0x00000002);
-  ASSERT_EQ(_DBG_BIT_MASK(31), 0x80000000);
-  ASSERT_EQ(_DBG_BIT_MASK(32), 0x00000001);
-  ASSERT_EQ(_DBG_BIT_MASK(63), 0x80000000);
-  ASSERT_EQ(_DBG_BIT_MASK(64), 0x00000001);
-  ASSERT_EQ(_DBG_BIT_MASK(95), 0x80000000);
-}
-
-TEST_F(DbgTest, DbgInitDestroy)
-{
-  // Verify data structure
-  dbg_ctx_t* dc = dbg_ctx_create(NULL, 1);
-  ASSERT_TRUE(dc->bits != NULL);
-  ASSERT_EQ(dc->bits[0], 0);
-  ASSERT_TRUE(dc->file == NULL);
-  dbg_ctx_destroy(dc);
-
-  dc = dbg_ctx_create(stdout, 1);
-  ASSERT_TRUE(dc->bits != NULL);
-  ASSERT_EQ(dc->bits[0], 0);
-  ASSERT_EQ(dc->file, stdout);
-  dbg_ctx_destroy(dc);
-}
-
-TEST_F(DbgTest, DbgCtxBitsInitToZero)
-{
-  const uint32_t num_bits = 65;
-  dbg_ctx_t* dc = dbg_ctx_create(NULL, num_bits);
-
-  // Verify all bits are zero
-  for(uint32_t bi = 0; bi < num_bits; bi++)
-  {
-    ASSERT_FALSE(dbg_gb(dc, bi));
-  }
-
-  // Verify bits array is zero
-  for(uint32_t i = 0; i < (num_bits + 31) / 32; i++)
-  {
-    ASSERT_EQ(dc->bits[i], 0);
-  }
-
-  dbg_ctx_destroy(dc);
-}
-
-TEST_F(DbgTest, TestWalkingOneBit)
-{
-  const uint32_t num_bits = 29;
-  dbg_ctx_t* dc = dbg_ctx_create(NULL, num_bits);
-
-  // Walking one bit
-  for(uint32_t bi = 0; bi < num_bits; bi++)
-  {
-    bool v = dbg_gb(dc, bi);
-    ASSERT_FALSE(v);
-    dbg_sb(dc, bi, 1);
-    v = dbg_gb(dc, bi);
-    ASSERT_TRUE(v);
-    
-    // Verify only one bit is set
-    for(uint32_t cbi = 0; cbi < num_bits; cbi++)
-    {
-      bool v = dbg_gb(dc, cbi);
-      if (cbi == bi)
-        ASSERT_TRUE(v);
-      else
-        ASSERT_FALSE(v);
-    }
-
-    // Verify only one bit is set using bits array
-    uint32_t bits_array_idx = bi / 32;
-    uint32_t bits_mask = 1 << (bi % 32);
-    for(uint32_t i = 0; i < (num_bits + 31) / 32; i++)
-    {
-      if(i == bits_array_idx)
-        ASSERT_EQ(dc->bits[i], bits_mask);
-      else
-        ASSERT_EQ(dc->bits[i], 0);
-    }
-    dbg_sb(dc, bi, 0);
-  }
-
-  dbg_ctx_destroy(dc);
-}
-
-TEST_F(DbgTest, TestWalkingTwoBits)
-{
-  const uint32_t num_bits = 147;
-  dbg_ctx_t* dc = dbg_ctx_create(NULL, num_bits);
-
-  // Number bits must be >= 2
-  ASSERT_TRUE(num_bits >= 2);
-
-  // Walking two bits
-  dbg_sb(dc, 0, 1);
-  for(uint32_t bi = 1; bi < num_bits - 1; bi++)
-  {
-    // Get first of the pair it should be 1
-    bool v = dbg_gb(dc, bi - 1);
-    ASSERT_TRUE(v);
-
-    // Get second bit it should be 0
-    v = dbg_gb(dc, bi);
-    ASSERT_FALSE(v);
-    
-    // Set second bit
-    dbg_sb(dc, bi, 1);
-
-    // Verify two bits are set
-    for(uint32_t cbi = 0; cbi < num_bits; cbi++)
-    {
-      bool v = dbg_gb(dc, cbi);
-      if (cbi == (bi - 1))
-        ASSERT_TRUE(v);
-      else if (cbi == bi)
-        ASSERT_TRUE(v);
-      else
-        ASSERT_FALSE(v);
-    }
-    
-    // Clear first bit
-    dbg_sb(dc, bi - 1, 0);
-  }
-
-  dbg_ctx_destroy(dc);
-}
-
-TEST_F(DbgTest, DbgReadWriteBitsOfDummy)
-{
-  dbg_ctx_t* dc = dbg_ctx_create(NULL, bits_size);
-
-  // Get bit index of dummy[0] and dummy[1] they should be adjacent
-  uint32_t bi0 = dbg_bnoi(dummy, 0);
-  uint32_t bi1 = dbg_bnoi(dummy, 1);
-  ASSERT_EQ(bi0 + 1, bi1);
-
-  // Initially they should be zero
-  bool o0 = dbg_gb(dc, bi0);
-  bool o1 = dbg_gb(dc, bi1);
-  ASSERT_FALSE(o0);
-  ASSERT_FALSE(o1);
-
-  // Write new values and verify they changed
-  dbg_sb(dc, bi0, !o0);
-  dbg_sb(dc, bi1, !o1);
-  ASSERT_EQ(dbg_gb(dc, bi0), !o0);
-  ASSERT_EQ(dbg_gb(dc, bi1), !o1);
-
-  // Restore original values
-  dbg_sb(dc, bi0, o0);
-  dbg_sb(dc, bi1, o1);
-  ASSERT_EQ(dbg_gb(dc, bi0), o0);
-  ASSERT_EQ(dbg_gb(dc, bi1), o1);
-
-  dbg_ctx_destroy(dc);
-}
 
 TEST_F(DbgTest, TestFmemopen)
 {
@@ -231,6 +50,145 @@ TEST_F(DbgTest, TestFmemopen)
   EXPECT_EQ(r, 0);
   EXPECT_EQ(strlen(buffer), 4);
   EXPECT_EQ(strcmp(buffer, "hi12"), 0);
+}
+
+TEST_F(DbgTest, DbgBitsArrayIdx)
+{
+  EXPECT_EQ(_DBG_BITS_ARRAY_IDX(0), 0);
+  EXPECT_EQ(_DBG_BITS_ARRAY_IDX(31), 0);
+  EXPECT_EQ(_DBG_BITS_ARRAY_IDX(32), 1);
+  EXPECT_EQ(_DBG_BITS_ARRAY_IDX(63), 1);
+  EXPECT_EQ(_DBG_BITS_ARRAY_IDX(64), 2);
+  EXPECT_EQ(_DBG_BITS_ARRAY_IDX(95), 2);
+}
+
+TEST_F(DbgTest, DbgBitMask)
+{
+  EXPECT_EQ(_DBG_BIT_MASK(0),  0x00000001);
+  EXPECT_EQ(_DBG_BIT_MASK(1),  0x00000002);
+  EXPECT_EQ(_DBG_BIT_MASK(31), 0x80000000);
+  EXPECT_EQ(_DBG_BIT_MASK(32), 0x00000001);
+  EXPECT_EQ(_DBG_BIT_MASK(63), 0x80000000);
+  EXPECT_EQ(_DBG_BIT_MASK(64), 0x00000001);
+  EXPECT_EQ(_DBG_BIT_MASK(95), 0x80000000);
+}
+
+TEST_F(DbgTest, DbgInitDestroy)
+{
+  // Verify data structure
+  dbg_ctx_t* dc = dbg_ctx_create(NULL, 1);
+  ASSERT_TRUE(dc->bits != NULL);
+  EXPECT_EQ(dc->bits[0], 0);
+  EXPECT_TRUE(dc->file == NULL);
+  dbg_ctx_destroy(dc);
+
+  dc = dbg_ctx_create(stdout, 1);
+  ASSERT_TRUE(dc->bits != NULL);
+  EXPECT_EQ(dc->bits[0], 0);
+  EXPECT_EQ(dc->file, stdout);
+  dbg_ctx_destroy(dc);
+}
+
+TEST_F(DbgTest, DbgCtxBitsInitToZero)
+{
+  const uint32_t num_bits = 65;
+  dbg_ctx_t* dc = dbg_ctx_create(NULL, num_bits);
+
+  // Verify all bits are zero
+  for(uint32_t bi = 0; bi < num_bits; bi++)
+  {
+    EXPECT_FALSE(dbg_gb(dc, bi));
+  }
+
+  // Verify bits array is zero
+  for(uint32_t i = 0; i < (num_bits + 31) / 32; i++)
+  {
+    EXPECT_EQ(dc->bits[i], 0);
+  }
+
+  dbg_ctx_destroy(dc);
+}
+
+TEST_F(DbgTest, TestWalkingOneBit)
+{
+  const uint32_t num_bits = 29;
+  dbg_ctx_t* dc = dbg_ctx_create(NULL, num_bits);
+
+  // Walking one bit
+  for(uint32_t bi = 0; bi < num_bits; bi++)
+  {
+    bool v = dbg_gb(dc, bi);
+    EXPECT_FALSE(v);
+    dbg_sb(dc, bi, 1);
+    v = dbg_gb(dc, bi);
+    EXPECT_TRUE(v);
+    
+    // Verify only one bit is set
+    for(uint32_t cbi = 0; cbi < num_bits; cbi++)
+    {
+      bool v = dbg_gb(dc, cbi);
+      if (cbi == bi)
+        EXPECT_TRUE(v);
+      else
+        EXPECT_FALSE(v);
+    }
+
+    // Verify only one bit is set using bits array
+    uint32_t bits_array_idx = bi / 32;
+    uint32_t bits_mask = 1 << (bi % 32);
+    for(uint32_t i = 0; i < (num_bits + 31) / 32; i++)
+    {
+      if(i == bits_array_idx)
+        EXPECT_EQ(dc->bits[i], bits_mask);
+      else
+        EXPECT_EQ(dc->bits[i], 0);
+    }
+    dbg_sb(dc, bi, 0);
+  }
+
+  dbg_ctx_destroy(dc);
+}
+
+TEST_F(DbgTest, TestWalkingTwoBits)
+{
+  const uint32_t num_bits = 147;
+  dbg_ctx_t* dc = dbg_ctx_create(NULL, num_bits);
+
+  // Number bits must be >= 2
+  EXPECT_TRUE(num_bits >= 2);
+
+  // Walking two bits
+  dbg_sb(dc, 0, 1);
+  for(uint32_t bi = 1; bi < num_bits - 1; bi++)
+  {
+    // Get first of the pair it should be 1
+    bool v = dbg_gb(dc, bi - 1);
+    EXPECT_TRUE(v);
+
+    // Get second bit it should be 0
+    v = dbg_gb(dc, bi);
+    EXPECT_FALSE(v);
+    
+    // Set second bit
+    dbg_sb(dc, bi, 1);
+
+    // Verify two bits are set
+    for(uint32_t cbi = 0; cbi < num_bits; cbi++)
+    {
+      bool v = dbg_gb(dc, cbi);
+      if (cbi == (bi - 1))
+        EXPECT_TRUE(v);
+      else if (cbi == bi)
+        EXPECT_TRUE(v);
+      else
+        EXPECT_FALSE(v);
+    }
+    
+    // Clear first bit
+    dbg_sb(dc, bi - 1, 0);
+  }
+
+  dbg_ctx_destroy(dc);
 }
 
 TEST_F(DbgTest, DbgPfu)
@@ -468,4 +426,46 @@ TEST_F(DbgTest, DbgPfeDbgPfx)
 
   dbg_ctx_destroy(dc);
   fclose(memfile);
+}
+
+TEST_F(DbgTest, DbgBnoi)
+{
+  EXPECT_EQ(dbg_bnoi(first,0), 0);
+  EXPECT_EQ(dbg_bnoi(first,1), 1);
+  EXPECT_EQ(dbg_bnoi(second,0), 30);
+  EXPECT_EQ(dbg_bnoi(second,1), 31);
+  EXPECT_EQ(dbg_bnoi(second,2), 32);
+  EXPECT_EQ(dbg_bnoi(another,0), 33);
+  EXPECT_EQ(dbg_bnoi(another,1), 34);
+  EXPECT_EQ(bits_size, 30 + 3 + 2);
+}
+
+TEST_F(DbgTest, DbgReadWriteBitsOfSecond)
+{
+  dbg_ctx_t* dc = dbg_ctx_create(NULL, bits_size);
+
+  // Get bit index of second[0] and second[1] they should be adjacent
+  uint32_t bi0 = dbg_bnoi(second, 0);
+  uint32_t bi1 = dbg_bnoi(second, 1);
+  EXPECT_EQ(bi0 + 1, bi1);
+
+  // Initially they should be zero
+  bool o0 = dbg_gb(dc, bi0);
+  bool o1 = dbg_gb(dc, bi1);
+  EXPECT_FALSE(o0);
+  EXPECT_FALSE(o1);
+
+  // Write new values and verify they changed
+  dbg_sb(dc, bi0, !o0);
+  dbg_sb(dc, bi1, !o1);
+  EXPECT_EQ(dbg_gb(dc, bi0), !o0);
+  EXPECT_EQ(dbg_gb(dc, bi1), !o1);
+
+  // Restore original values
+  dbg_sb(dc, bi0, o0);
+  dbg_sb(dc, bi1, o1);
+  EXPECT_EQ(dbg_gb(dc, bi0), o0);
+  EXPECT_EQ(dbg_gb(dc, bi1), o1);
+
+  dbg_ctx_destroy(dc);
 }
